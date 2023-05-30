ba99ef14c7542d07b117fbe1c9de7ecaee9bc88d
Navau <milibrolunadepluton@gmail.com>
2023-05-29 20:41:52 -0400

    29/05/23

--- products/api/views.py
+++ products/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from products.models import Product
 from products.api.serializers import ProductSerializer
@@ -11,8 +12,17 @@ class ProductApiViewSet(ModelViewSet):
     permission_classes = []
     serializer_class = ProductSerializer
     queryset = Product.objects.all().order_by("id")
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ["category", "deposit"]
+    search_fields = [
+        "name",
+        "description",
+        "price",
+        "stock",
+        "update_at",
+        "category__name",
+        "deposit__name",
+    ]
 
     def create(self, request, *args, **kwargs):
         print("REQUEST_DATA", request.data)
@@ -21,3 +31,6 @@ class ProductApiViewSet(ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         print("REQUEST_DATA", request.data)
         return super().partial_update(request, *args, **kwargs)
+
+    # def pre_save(self, obj):
+    #     obj.update_at = timezone.now()
